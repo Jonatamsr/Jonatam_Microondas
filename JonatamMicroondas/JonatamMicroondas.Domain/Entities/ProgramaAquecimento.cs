91b8f54062dd47bc69337c306b80d47149d0ed2d
Jonatam Rocha <jonatamsr@hotmail.com>
2019-07-08 23:34:12 -0300

    Criando pastas de Solução, pasta Entities e a entidade "ProgramaAquecimento".

---
+++ JonatamMicroondas/JonatamMicroondas.Domain/Entities/ProgramaAquecimento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JonatamMicroondas.Domain.Entities
+{
+    class ProgramaAquecimento
+    {
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+        public DateTime Tempo { get; set; }
+        public int Potencia { get; set; }
+    }
+}
